e3aa01e39d286669084e711b0179e44799eab552
Renu Punjabi <rpunjabi12@gmail.com>
2017-10-29 17:39:07 -0700

    getting current location

--- FoodDelivery/ViewControllers/MapViewController.swift
+++ FoodDelivery/ViewControllers/MapViewController.swift
@@ -6,9 +6,57 @@
 //  Copyright © 2017 Renu Punjabi. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    let locationManager = CLLocationManager()
+    var myLocation: CLLocationCoordinate2D?
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpLocationManager()
+    }
+    
+    fileprivate func setUpLocationManager() {
+        //Ask for authorization from user
+  //      locationManager.requestAlwaysAuthorization()
+        
+        //Ask for permission, when in foreground
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        if let coordinates = mapView.userLocation.location?.coordinate {
+            mapView.setCenter(coordinates, animated: true)
+        }
+    }
+    
+    // MARK - CLLocationManagerDelegate
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = self.locationManager.location?.coordinate {
+            centerMap(coordinates: location)
+        }
+    }
+    
+    fileprivate func centerMap(coordinates: CLLocationCoordinate2D) {
+        myLocation = coordinates
+        let spanX = 0.007
+        let spanY = 0.007
+        
+        let region = MKCoordinateRegion(center: coordinates, span: MKCoordinateSpanMake(spanX, spanY))
+        mapView.setRegion(region, animated: true)
+    }
 }
