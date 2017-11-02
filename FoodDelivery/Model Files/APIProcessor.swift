84a325b7295a7de356bd03a0db3e6ba4aae90227
Renu Punjabi <rpunjabi12@gmail.com>
2017-11-02 13:10:12 -0700

    added apiProcessor file

---
+++ FoodDelivery/Model Files/APIProcessor.swift
@@ -0,0 +1,13 @@
+//
+//  APIProcessor.swift
+//  FoodDelivery
+//
+//  Created by Renu Punjabi on 11/2/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+import Foundation
+
+class APIProcessor: NSObject {
+    <#code#>
+}
