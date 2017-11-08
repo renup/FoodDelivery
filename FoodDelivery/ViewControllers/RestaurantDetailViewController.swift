b12aa48ecbbffc707068a8efac2e59e2f034d3d5
Renu Punjabi <rpunjabi12@gmail.com>
2017-11-08 14:21:24 -0800

    added restaurantDetailVC in storyboard

---
+++ FoodDelivery/ViewControllers/RestaurantDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodDelivery
+//
+//  Created by Renu Punjabi on 11/8/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+    
+}
