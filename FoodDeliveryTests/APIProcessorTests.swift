9dd0924066c3658fd75c2e1df0e78f3d6ae59495
Renu Punjabi <rpunjabi12@gmail.com>
2017-11-16 06:31:49 -0800

    xcode settings to use 3.2 swift language for mockingjay

---
+++ FoodDeliveryTests/APIProcessorTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIProcessorTests.swift
+//  FoodDeliveryTests
+//
+//  Created by Renu Punjabi on 11/16/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Mockingjay
+
+@testable import FoodDelivery
+
+class APIProcessorTests: QuickSpec {
+    
+}
